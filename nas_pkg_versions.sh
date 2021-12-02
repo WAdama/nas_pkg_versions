b43f1de80684dc15afdea4532de1fd8d10d2ffcd
Ingo <foren@asche-rz.de>
2021-12-02 14:00:16 +0100

    Added installed version to text field

--- nas_pkg_versions.sh
+++ nas_pkg_versions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Version 1.0
+#Version 1.0.1
 
 declare -a PACKAGES
 TEXT=""
@@ -13,10 +13,10 @@ else
 for PACKAGE in ${PACKAGES[@]}
 do
 	NAME=$(echo $PACKAGE | jq -r .id)
+	INSTALLED=$(synopkg version $NAME)
 	VERSION=$(echo $PACKAGE | jq -r .version)
-	TEXT="$TEXT $NAME ($VERSION) |"
+	TEXT="$TEXT $NAME ($INSTALLED -> $VERSION) |"
 done
-#TEXT="Update(s) available for: $(echo ${TEXT[@]} | awk -v OFS=" / " '{$1=$1;print}')"
 TEXT=$(echo ${TEXT[@]} | sed 's/..$//')
 fi
 echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Updates?</channel><value>$UPDATES</value><ValueLookup>prtg.standardlookups.nas.packages</ValueLookup><ShowChart>0</ShowChart></result><text>$TEXT</text></prtg>"
